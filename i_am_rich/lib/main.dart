da6b1be32d7542015456bb94fbcea7451f685ede
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-15 23:39:45 -0500

    Add image to body

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -10,6 +10,9 @@ void main() {
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
+        body: Image(
+          image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+        ),
       ),
     ),
   );
