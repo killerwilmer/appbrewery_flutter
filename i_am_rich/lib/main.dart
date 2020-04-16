1c098351f1c18ef4d5336a2929a5a3fbf0bf9776
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-15 22:37:16 -0500

    Add Scaffold, AppBar, and Title

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello world',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
         ),
       ),
     ),
