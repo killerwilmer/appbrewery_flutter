f2a27c262c5c731785a74d73239f1c70dbab64d4
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-17 15:52:16 -0500

    Add margin to Container

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
           child: Container(
             height: 100.0,
             width: 100.0,
+            margin: EdgeInsets.all(20.0),
             color: Colors.white,
             child: Text('Hello'),
           ),
