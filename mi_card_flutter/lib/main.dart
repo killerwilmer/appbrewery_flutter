750c289fc2b171496cd383a9b6233a10bb9ad0b0
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-17 15:54:46 -0500

    Add padding to Container

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
             height: 100.0,
             width: 100.0,
             margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(10.0),
             color: Colors.white,
             child: Text('Hello'),
           ),
