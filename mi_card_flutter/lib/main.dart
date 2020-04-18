3bbb7f87137567a745ae2469a4751d1929e56d30
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-18 16:17:34 -0500

    SizedBox, to create space inter Containers

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -19,6 +19,9 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
                 child: Text('Container 1'),
               ),
+              SizedBox(
+                height: 20.0,
+              ),
               Container(
                 height: 100.0,
                 color: Colors.blue,
