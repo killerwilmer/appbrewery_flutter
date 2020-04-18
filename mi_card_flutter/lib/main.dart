28843a080044bc590795be5ff061d17428b01ded
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-18 15:56:15 -0500

    Same behavior for horizontal axis

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -16,25 +16,22 @@ class MyApp extends StatelessWidget {
             children: <Widget>[
               Container(
                 height: 100.0,
-                width: 100.0,
+                width: double.infinity,
                 color: Colors.white,
                 child: Text('Container 1'),
               ),
               Container(
-                width: 100.0,
+                width: double.infinity,
                 height: 100.0,
                 color: Colors.blue,
                 child: Text('Container 2'),
               ),
               Container(
-                width: 100.0,
+                width: double.infinity,
                 height: 100.0,
                 color: Colors.red,
                 child: Text('Container 3'),
               ),
-              Container(
-                width: double.infinity,
-              )
             ],
           ),
         ),
