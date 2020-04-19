1cb12dd66acdaa0508555919193de319675e6ec7
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-18 21:47:04 -0500

    Use font in Text

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
               Text(
                 'Wilmer Arteaga',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
