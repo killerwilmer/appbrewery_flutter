ee5ccbd6a4afa7fc5a56edd1beeae67f909f941d
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-19 16:04:26 -0500

    Add another Expanded with property flex
    Property flex distributes the space depending the number

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -21,8 +21,15 @@ class DicePage extends StatelessWidget {
     return Row(
       children: <Widget>[
         Expanded(
+          flex: 2,
           child: Image(
             image: AssetImage('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image(
+            image: AssetImage('images/dice2.png'),
           ),
         ),
       ],
