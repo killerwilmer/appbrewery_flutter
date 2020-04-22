75b2940e36299975c483537aabc0018f58037cb7
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-22 15:47:36 -0500

    Change StatelessWidget to StatefulWidget
    In intellij you can write stful and ENTER for create the structure for StatefulWidget

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -15,13 +15,16 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  int leftDiceNumber;
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-    leftDiceNumber = 2;
-
     return Center(
       child: Row(
         children: <Widget>[
