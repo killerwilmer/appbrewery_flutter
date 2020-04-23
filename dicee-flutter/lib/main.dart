f31a0eaa7de56dfd023e86555479c16acd67077a
Wilmer Arteaga <killerwilmer@gmail.com>
2020-04-22 19:29:03 -0500

    Create rightDiceNumber with random number

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -23,6 +23,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -42,9 +43,11 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Right button got pressed');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
