ecfbee95a5bba3517ad8958d5215af5f4dd0e69f
Wilmer Arteaga <killerwilmer@gmail.com>
2020-05-02 23:56:02 -0500

    TODO: Step 4 - Create a Constructor for Story class.
    TODO: Step 4 - Create a Constructor for this class to be able to initialize the properties created in step 3.

--- destini-challenge-starting/lib/story.dart
+++ destini-challenge-starting/lib/story.dart
@@ -2,6 +2,6 @@ class Story {
   String storyTitle;
   String choice1;
   String choice2;
-}
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story(this.storyTitle, this.choice1, this.choice2);
+}
