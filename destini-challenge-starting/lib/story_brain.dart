ad18dd49104f14e8d1da7d1fc52482bbc302ee01
Wilmer Arteaga <killerwilmer@gmail.com>
2020-05-03 22:44:34 -0500

    TODO: Step 23 - Use the storyNumber
    TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.

--- destini-challenge-starting/lib/story_brain.dart
+++ destini-challenge-starting/lib/story_brain.dart
@@ -36,15 +36,15 @@ class StoryBrain {
   ];
 
   String getStory() {
-    return _storyData[0].storyTitle;
+    return _storyData[storyNumber].storyTitle;
   }
 
   String getChoice1() {
-    return _storyData[0].choice1;
+    return _storyData[storyNumber].choice1;
   }
 
   String getChoice2() {
-    return _storyData[0].choice2;
+    return _storyData[storyNumber].choice2;
   }
 
   void nextStory(int choiceNumber) {
@@ -71,8 +71,6 @@ class StoryBrain {
     storyNumber = 0;
   }
 }
-
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
