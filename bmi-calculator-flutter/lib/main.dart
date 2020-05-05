145d0bebdd0d13e6152f7a1757bad7e9e67ae1b3
Wilmer Arteaga <killerwilmer@gmail.com>
2020-05-04 23:31:22 -0500

    Use hexadecimal colors

--- bmi-calculator-flutter/lib/main.dart
+++ bmi-calculator-flutter/lib/main.dart
@@ -7,8 +7,12 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.red,
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
         accentColor: Colors.purple,
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.white),
+        ),
       ),
       home: InputPage(),
     );
