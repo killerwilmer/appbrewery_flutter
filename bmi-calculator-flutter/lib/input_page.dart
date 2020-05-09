61ef9f7c9d683fd74ecd26fbf95b91ca44168d16
Wilmer Arteaga <killerwilmer@gmail.com>
2020-05-08 20:58:10 -0500

    Create reusable widget IconContent

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -77,6 +77,36 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  final String labelCard;
+  final IconData iconCard;
+
+  IconContent({this.labelCard, this.iconCard});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconCard,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          labelCard,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   final Color colour;
   final Widget cardChild;
