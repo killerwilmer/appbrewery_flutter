8bd33d7894b8c4cef02bd02fc948d56e941dc482
Wilmer Arteaga <killerwilmer@gmail.com>
2020-05-13 08:08:42 -0500

    Create directories for screens and components

--- bmi-calculator-flutter/lib/screens/input_page.dart
+++ bmi-calculator-flutter/lib/screens/input_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'reusable_card.dart';
-import 'icon_content.dart';
-import 'constants.dart';
-import 'bottom_button.dart';
-import 'round_icon_button.dart';
+import '../components/reusable_card.dart';
+import '../components/icon_content.dart';
+import '../constants.dart';
+import '../components/bottom_button.dart';
+import '../components/round_icon_button.dart';
 
 enum Gender { female, male }
 
