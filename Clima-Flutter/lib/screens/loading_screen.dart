b7e85cbde4d3804b5b8f7fe18e5b97aaa1f5deef
Wilmer Arteaga <killerwilmer@gmail.com>
2020-05-19 12:48:48 -0500

    Use flutter_spinkit while is loading and Navigator to LocationScreen

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'xxx';
 
@@ -21,7 +23,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 
   @override
@@ -41,5 +50,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 }
